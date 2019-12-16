4c830f7583bcf88eba1de1652f82fda2cc037854
Ms <ms@juliver.com>
2019-12-16 21:10:32 +0100

    Rédaction PageShoppingCart

--- src/main/java/ms/fr/PageShoppingCart.java
+++ src/main/java/ms/fr/PageShoppingCart.java
@@ -1,19 +1,29 @@
 package ms.fr;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class PageShoppingCart {
 	@FindBy (name="lines[0].quantity") 
-	WebElement quantity_field;
+	private WebElement quantity_field;
 	
 	@FindBy (name="update") 
-	WebElement bouton_update;
+	private WebElement bouton_update;
 	
 	@FindBy (xpath="//tr[2]/td[6]/span") 
-	WebElement value_total;
+	private WebElement value_total;
 	
 	@FindBy (xpath="//tr[2]/td[7]/span") 
-	WebElement value;
+	private WebElement value;
+	
+	public PageShoppingCart changementQuantite(WebDriver driver, int i) {
+		quantity_field.clear();
+		String s = Integer.toString(i);
+		quantity_field.sendKeys(s);
+		bouton_update.click();
+		return PageFactory.initElements(driver, PageShoppingCart.class);
+	}
 
 }
